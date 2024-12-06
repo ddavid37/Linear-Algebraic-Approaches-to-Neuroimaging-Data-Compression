2cf9e8d4d66ce2a0d0fca2fd105a5fa0c3db51cf
Daniel David <ddavid37@montgomerycollege.edu>
2024-12-06 10:38:45 -0500

    Add SVD approximation visualization to SVD_001.py

--- SVD_001.py
+++ SVD_001.py
@@ -19,3 +19,18 @@ img = plt.imshow(X)
 img.set_cmap('gray')
 plt.axis('off')         
 plt.show()
+
+U, S, VT = np.linalg.svd(X,full_matrices=False)
+S = np.diag(S)
+
+j = 0
+for r in (5, 10, 20, 30, 40, 50, 75, 100):
+    # Construct approximate image
+    Xapprox = U[:,:r] @ S[0:r,:r] @ VT[:r,:]
+    plt.figure(j+1)
+    j += 1
+    img = plt.imshow(Xapprox)
+    img.set_cmap('gray')
+    plt.axis('off')
+    plt.title('r = ' + str(r))
+    plt.show()
